3b1694ebd0f1ea1ed0e875e58cd27efdec40d637
Olev Abel <olev@tamiatics.com>
2025-10-15 17:51:09 +0300

    Fix application navCoordinator singleton issue

--- app/src/main/java/ee/ut/cs/shoppinglist/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/shoppinglist/MainActivity.kt
@@ -4,17 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import ee.ut.cs.shoppinglist.ui.screens.ShoppingListScreen
 import ee.ut.cs.shoppinglist.ui.theme.ShoppingListTheme
-import ee.ut.cs.shoppinglist.ui.viewmodels.AddItemViewModel
-import ee.ut.cs.shoppinglist.ui.viewmodels.ShoppingListViewModel
 
 
 class MainActivity : ComponentActivity() {
